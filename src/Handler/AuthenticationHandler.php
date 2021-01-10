6980634f9846ee1a7ce67aee987c341973036535
Artur <galimborjogos@gmail.com>
2021-01-10 17:05:58 +0000

    Final update v2

--- src/Handler/AuthenticationHandler.php
+++ src/Handler/AuthenticationHandler.php
@@ -11,8 +11,10 @@ use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
 use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class AuthenticationHandler implements  LogoutSuccessHandlerInterface
+class AuthenticationHandler extends AbstractController implements  LogoutSuccessHandlerInterface
 {
 
 
@@ -22,8 +24,8 @@ class AuthenticationHandler implements  LogoutSuccessHandlerInterface
 
         $referer = $request->headers->get('referer');
         $request->getSession()->getFlashBag()->add('success', 'You have successfully logged out.');
-        return new RedirectResponse($request->getBaseUrl());
+//        return new RedirectResponse($request->getBaseUrl() );
 //       return new RedirectResponse($referer);
-
+        return $this->redirectToRoute('index');
     }
 }
